11bb28e77d0c087035167f180a4e28ff5a6d9c87
arcenioCorteis <arcenioCorteis@refera.com.br>
2024-07-31 10:36:33 +0200

    feat- Remoção de favoritos...

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,10 +10,20 @@ function Favoritos() {
     setFilmes(JSON.parse(myFavorite) || []);
   }, []);
 
+  function excluirFilmes(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+  }
+
   return (
     <div className="meus-filmes">
       <h1>Filmes Favoritos</h1>
-
+      {filmes.length === 0 && (
+        <span>Você não possui nenhum filme favorito :( !!</span>
+      )}
       <ul>
         {filmes.map((item) => {
           return (
@@ -21,7 +31,7 @@ function Favoritos() {
               <span>{item.title}</span>
               <div>
                 <Link to={`/filme/${item.id}`}> Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilmes(item.id)}>Excluir</button>
               </div>
             </li>
           );
